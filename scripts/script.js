d36e0ba11d5c13732325e9bfb93fbe03c1dac820
Konstantin_999 <sh-tov@yandex.ru>
2021-10-11 02:28:26 +0700

    feature: добавить адаптивность на разрешениях до мобильного переключения в блокe bikes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
 
-
-  
   const slides = new Swiper(".surfaceAndGradients__slides", {
     navigation: {
       nextEl: ".surfaceAndGradients__button-slide_next",
@@ -20,9 +18,7 @@
     },
     grabCursor: true,
     slideToClikedSlide: true,
-
   });
-
 
   const bikesGroup = ["Шоссе", "Грэвел", "TT"];
   const slidesBikes = new Swiper(".bikes__slides", {
@@ -35,65 +31,53 @@
       },
     },
     effect: "slide",
-    // effect: "fade",
-    // speed: 700,
-    // fadeEffect: { crossFade: true },
-    simulateTouch: false,
     slidesPerView: 1,
-    // wateOverflow: true,
     slidesPerGroup: 1,
-    // loop: true,
   });
   
-  
-  
-  
   const slidesHighway = new Swiper(".highway-slides", {
-    navigation: {
-      nextEl: ".highway-slides__prev",
-      prevEl: ".highway-slides__next",
-    },
     pagination: {
       el: ".highway-slides__swiper-pagination",
       clickable: true,
     },
-    
-    slidesPerView: 3,
-    // wateOverflow: true,
+    slidesPerView: 1,
+    wateOverflow: true,
     spaceBetween: 30,
     slidesPerGroup: 1,
-    loop: true,
     nested: true,
+    breakpoints: {
+      600: { slidesPerView: 3 },
+     
+    },
   });
   const slidesGravel = new Swiper(".gravel-slides", {
     spaceBetween: 30,
     pagination: {
       el: ".gravel-slides__swiper-pagination",
       clickable: true,
-      dynamicBullets: true,
-      // renderBullet: function (index, className) {
-        //   return '<span class="' + className + '">' + bikesGroup[index] + "</span>";
-        // },
       },
-      slidesPerView: 3,
+      slidesPerView: 1,
       wateOverflow: true,
       slidesPerGroup: 1,
-      loop: true,
+      nested: true,
+      breakpoints: {
+        600: { slidesPerView: 3 },
+       
+      },
     });
     const slidesTT = new Swiper(".TT-slides", {
       spaceBetween: 30,
       pagination: {
         el: ".TT-slides__swiper-pagination",
         clickable: true,
-        dynamicBullets: true,
-        // renderBullet: function (index, className) {
-          //   return '<span class="' + className + '">' + bikesGroup[index] + "</span>";
-          // },
         },
-        slidesPerView: 3,
+        slidesPerView: 1,
         wateOverflow: true,
         slidesPerGroup: 1,
-        loop: true,
+        breakpoints: {
+          600: { slidesPerView: 3 },
+         
+        },
       });
       
       console.log ('end');
